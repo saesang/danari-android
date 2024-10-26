d3c4bed969f3dda2a04641bd89a60412fe96ad51
탁세하 <takseha98@gmail.com>
2024-10-26 21:21:48 +0900

    feat(#5): 동아리 메인 화면 UI 구현

---
+++ android/app/src/main/java/com/takseha/danari/presentation/ui/CircleHomeActivity.kt
@@ -0,0 +1,21 @@
+package com.takseha.danari.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.takseha.danari.R
+
+class CircleHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_circle_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
